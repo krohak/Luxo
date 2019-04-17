d8db5b2ae8b362b530dac1120a41edc77781572f
krohak <rohaksinghal14@gmail.com>
2019-04-17 14:56:56 +0800

    discovering the lamp

--- yeelight/index.js
+++ yeelight/index.js
@@ -15,11 +15,26 @@ y.on('deviceadded', function(device) {
 y.on('deviceconnected', function(device) {
 	console.log('device connected');
 
-	var state = true;
+	var state = 0;
+	var brightness = 100;
+	colors = [
+		[255, 255, 0],
+		[0, 255, 255],
+		[255, 0, 255]
+	]
 	setInterval(function() {
-		y.setPower(device, state, 300);
+		// y.setPower(device, state, 300);
+		// state = state ? false : true;
 
-		state = state ? false : true;
+		console.log(brightness)
+
+		y.setBrightness(device, brightness, 300);
+		y.setRGB(device, colors[state] ,300)
+
+		brightness = brightness == 0? 100 : brightness-10;
+		
+		
+		state = (state+1)%3;
 	}, 3000);
 });
 
